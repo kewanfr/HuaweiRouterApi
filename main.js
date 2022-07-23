3bd4b291b231b67dfdcb3d106e8f176c76bfde84
Kéwan Bernier <mail@kewan.fr>
2022-07-23 19:23:56 +0200

    code

---
+++ main.js
@@ -0,0 +1,21 @@
+var router = require('@p4d/huawei-router')
+require('dotenv').config()
+
+router.setHost('192.168.2.252')
+router.setUsername('admin')
+router.setPassword(process.env.PASSWORD)
+
+var networkType = {
+  0: "Aucun Service",
+  4: "3G",
+  19: "4G",
+}
+
+router.getStatus().then((data) => {
+  var result = {
+    Signal: data.SignalIcon,
+    NetworkType: networkType[data.CurrentNetworkType],
+    datas: data
+  }
+  console.log(JSON.stringify(result));
+})
