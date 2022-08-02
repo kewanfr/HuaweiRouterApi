9750fdbef319573b9a6737f345249fbe01fe4fa3
Kéwan Bernier <mail@kewan.fr>
2022-08-02 20:31:46 +0200

    Signal

--- main.js
+++ main.js
@@ -7,8 +7,23 @@ router.setPassword(process.env.PASSWORD)
 
 var networkType = {
   0: "Aucun Service",
+  2: "Edge",
+  3: "Edge",
   4: "3G",
+  5: "3G",
+  6: "3G",
+  7: "3G",
+  8: "3G",
   9: "3G",
+  10: "3G",
+  11: "3G",
+  12: "3G",
+  13: "3G",
+  14: "3G",
+  15: "3G",
+  16: "3G",
+  17: "3G",
+  18: "3G",
   19: "4G",
 }
 
